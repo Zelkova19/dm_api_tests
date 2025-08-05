9320a8fcd41200fdb38b9ff25d58988841c060f4
zelkova <cohiba666@gmail.com>
2025-08-05 16:34:57 +0300

    first test Сделаны проверки Сделан код получения активации

--- test_post_v1_account.py
+++ test_post_v1_account.py
@@ -1,10 +1,13 @@
+import pprint
+
 import requests
 
+from json import loads
 
 def test_post_v1_account():
     # Регистрация полльзователя
 
-    login = 'vmenshikov_test'
+    login = 'vmenshikov_test113'
     password = '123456789'
     email = f'{login}@mail.ru'
     json_data = {
@@ -16,32 +19,42 @@ def test_post_v1_account():
     response = requests.post('http://5.63.153.31:5051/v1/account', json=json_data)
     print(response.status_code)
     print(response.text)
+    assert response.status_code == 201, f'Пользователь не был создан {response.json()}'
 
     # Получить письма из почтового сервера
 
     params = {
-        'limit': '50'
+        'limit': '5'
     }
 
     response = requests.get('http://5.63.153.31:5025/api/v2/messages', params=params, verify=False)
     print(response.status_code)
     print(response.text)
+    assert response.status_code == 200, f'письмо не получено'
 
     #  Получить активационный токен
+    user_token = None
+    for item in response.json()['items']:
+        user_data = loads(item['Content']['Body'])
+        user_login = user_data['Login']
+        if user_login == login:
+            user_token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+            print(user_token)
 
-    ...
+    assert user_token is not None
 
     #  Активация пользователя
     headers = {
         'accept': 'test/plain'
     }
 
-    response = requests.get('http://5.63.153.31:5051/v1/account/dd4ed1b4-0f8c-4e65-8447-271fb81eeee1', headers=headers)
+    response = requests.put(f'http://5.63.153.31:5051/v1/account/{user_token}', headers=headers)
     print(response.status_code)
     print(response.text)
+    assert response.status_code == 200, f'Пользователь не был активирован'
 
     # Авторизоваться
-
+    #
     json_data = {
         'login': login,
         'password': password,
@@ -49,7 +62,8 @@ def test_post_v1_account():
     }
 
 
-    response = requests.get('http://5.63.153.31:5051/v1/account/dd4ed1b4-0f8c-4e65-8447-271fb81eeee1', json=json_data)
+    response = requests.post(f'http://5.63.153.31:5051/v1/account/login', json=json_data)
     print(response.status_code)
     print(response.text)
+    assert response.status_code == 200, f'Пользователь не был авторизован'
 
