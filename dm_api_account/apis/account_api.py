e0411f97f8d8f9c7f00642bc8f439dd4c0728b54
zelkova <cohiba666@gmail.com>
2025-08-05 23:17:22 +0300

    Add class clients

---
+++ dm_api_account/apis/account_api.py
@@ -0,0 +1,45 @@
+import requests
+
+
+class AccountApi:
+
+    def __init__(
+            self,
+            host,
+            headers=None
+    ):
+        self.host = host
+        self.headers = headers
+
+    def post_v1_account(
+            self,
+            json_data
+    ):
+        """
+        Register new user
+        :param json_data:
+        :return:
+        """
+        response = requests.post(
+            url=f'{self.host}/v1/account',
+            json=json_data
+        )
+        return response
+
+    def put_v1_account_token(
+            self,
+            user_token
+    ):
+        """
+        Activate registered user
+        :param user_token:
+        :return:
+        """
+        headers = {
+            'accept': 'test/plain'
+        }
+        response = requests.put(
+            url=f'{self.host}/v1/account/{user_token}',
+            headers=headers
+        )
+        return response
---
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -0,0 +1,71 @@
+from json import loads
+
+from dm_api_account.apis.account_api import AccountApi
+from dm_api_account.apis.login_api import LoginApi
+from api_mailhog.apis.mailhog_api import MailhogApi
+
+
+def test_post_v1_account():
+    # Регистрация полльзователя
+    account_api = AccountApi(host='http://5.63.153.31:5051/')
+    login_api = LoginApi(host='http://5.63.153.31:5051/')
+    mailhog_api = MailhogApi(host='http://5.63.153.31:5025/')
+
+
+    login = 'vmenshikov_test_ro_4'
+    password = '123456789'
+    email = f'{login}@mail.ru'
+    json_data = {
+        'login': login,
+        'email': email,
+        'password': password
+    }
+
+    response = account_api.post_v1_account(json_data=json_data)
+    print(response.status_code)
+    print(response.text)
+    assert response.status_code == 201, f'Пользователь не был создан {response.json()}'
+
+    # Получить письма из почтового сервера
+
+    response = mailhog_api.get_api_v2_messages()
+    print(response.status_code)
+    print(response.text)
+    assert response.status_code == 200, f'Письмо не получено'
+
+    #  Получить активационный токен
+
+    user_token = get_activation_token_by_login(login, response)
+
+    assert user_token is not None, 'Ожидали токен, получили None'
+
+    #  Активация пользователя
+
+    response = account_api.put_v1_account_token(user_token=user_token)
+    print(response.status_code)
+    print(response.text)
+    assert response.status_code == 200, f'Пользователь не был активирован'
+
+    # Авторизоваться
+
+    json_data = {
+        'login': login,
+        'password': password,
+        'rememberMe': True
+    }
+
+    response = login_api.post_v1_account_login(json_data=json_data)
+    print(response.status_code)
+    print(response.text)
+    assert response.status_code == 200, f'Пользователь не был авторизован'
+
+
+def get_activation_token_by_login(login, response):
+    user_token = None
+    for item in response.json()['items']:
+        user_data = loads(item['Content']['Body'])
+        user_login = user_data['Login']
+        if user_login == login:
+            user_token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+            print(user_token)
+    return user_token
