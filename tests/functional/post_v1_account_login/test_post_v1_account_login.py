68f14b79726545378404f7468b0ebcbad9d732d9
zelkova <cohiba666@gmail.com>
2025-08-07 09:09:34 +0300

    Fix tests

--- tests/functional/post_v1_account_login/test_post_v1_account_login.py
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -13,7 +13,7 @@ def test_post_v1_account_login():
 
     # Подготовка пользователя
 
-    login = 'vmenshikov_test_ro_50'
+    login = 'vmenshikov_test_ro_51'
     password = '123456789'
     email = f'{login}@mail.ru'
     json_data = {
